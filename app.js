2344d0acf8fdc843ce8ee03b27b3ad20f066c299
hadeelgamal <hadeel.abdelkarim@gmail.com>
2022-12-13 19:31:57 +0200

    done

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 // Register the location for handlebars partials here:
 
 // ...
@@ -19,7 +18,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
+  console.log('get root');
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  const beers = punkAPI.getBeers();
+  beers.then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+      res.render('beers', { allBeers: beersFromApi });
+    })
+
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  const randomBeer = punkAPI.getRandom();
+  randomBeer.then(randomBeerFromApi => {
+      console.log('Beers from the database: ', randomBeerFromApi[0] );
+      res.render('random-beer', { randomBeer: randomBeerFromApi[0] });
+    })
+
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
